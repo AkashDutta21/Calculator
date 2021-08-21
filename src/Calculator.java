c0beca8bf31699426f9ece046f2230c91dc87707
Akash Dutta <Akash.Dutta2@mindtree.com>
2021-08-21 12:25:49 +0530

    Initial Commit

---
+++ src/Calculator.java
@@ -0,0 +1,22 @@
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float a=10;
+		float b=20;
+		
+		System.out.println(mul(a, b));
+		System.out.println(div(a, b));
+	}
+	
+	public static float mul(float a, float b) {
+		return a*b;
+	}
+	
+	public static float div(float a, float b) {
+		return a/b;
+	}
+}
+
+
